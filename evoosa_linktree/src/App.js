a12516f010a042df53cd028636195887d6bd8588
evoosa <eva@scytale.ai>
2022-08-07 21:50:20 +0300

    initial commit

---
+++ evoosa_linktree/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className = "App" >
+            <div className = "container row">
+                {
+                    items.map((item, i) => {
+                        return(
+                            ninini
+                        )
+                    })
+                }
+            </div>
+        </div>
+    );
+}
+
+export default App;
